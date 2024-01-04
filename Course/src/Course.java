8226cdd490a08db606db22e4281fa9dee0381e90
Saleh-Binessa <83325@kfh.com>
2024-01-04 10:29:56 +0300

    OOP task

---
+++ Course/src/Course.java
@@ -0,0 +1,46 @@
+public class Course {
+
+    String courseName;
+    String InsName;
+    int NumOfStudentsEnrolled;
+
+    Course(String courseNameRecieved, String InsNameRecieved, int NumOfStudentsEnrolledRecieved){
+        courseName = courseNameRecieved;
+        InsName = InsNameRecieved;
+        NumOfStudentsEnrolled = NumOfStudentsEnrolledRecieved;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public void setCourseName(String newCourseName){
+        courseName = newCourseName;
+    }
+    public String getInseName(){
+        return InsName;
+    }
+
+    public void setInsName(String newInsName){
+        InsName = newInsName;
+
+    }
+    public int getNumOfStudentsEnrolled(){
+        return NumOfStudentsEnrolled;
+    }
+
+    public void setNumOfStudentsEnrolled(int newNumOfStudentsEnrolled){
+        NumOfStudentsEnrolled = newNumOfStudentsEnrolled;
+    }
+@Override
+public String toString() {
+    return " Course Name: " + courseName + "\n Instructor Name: " + InsName + "\n Number Of Students Enrolled: " + 25;
+
+
+}
+    public static void main(String[] args) throws Exception {
+    
+        Course firstCourse = new Course("Math", "Ahmad", 25);
+        System.out.println(firstCourse.toString());
+    }
+}
